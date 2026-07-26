c8c6faf174976261b1ac8be61a63f1d92d3dd667
Parul Verma <parulverma4399@gmail.com>
2026-07-26 23:38:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.java
@@ -0,0 +1,20 @@
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+}*/
+
+class Solution {
+
+    int countLeaves(Node root) {
+
+        if (root == null)
+            return 0;
+
+        if (root.left == null && root.right == null)
+            return 1;
+
+        return countLeaves(root.left) + countLeaves(root.right);
+    }
+}
